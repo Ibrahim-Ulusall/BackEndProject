a974d8f8baa42c34551e14aa63cf3d72ec62d152
ibrahim <ibrahimulusal185@gmail.com>
2023-05-27 21:56:38 +0300

    NorthwindContext Class created

--- DataAccsess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccsess/Concrete/EntityFramework/EfProductDal.cs
@@ -7,7 +7,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccsess.Concrete.Entity_Framework
+namespace DataAccsess.Concrete.EntityFramework
 {
     public class EfProductDal : IProductDal
     {
