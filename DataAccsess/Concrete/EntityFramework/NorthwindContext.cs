d7635519cae5f23bb2de432267bba8e246ece7a6
ibrahim <ibrahimulusal185@gmail.com>
2023-05-27 22:15:49 +0300

    VeriTabanı Projeye Dahil Edildi.

--- DataAccsess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccsess/Concrete/EntityFramework/NorthwindContext.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataAccsess.Concrete.EntityFramework
 {
-    public class NorthwindContext:DbContext
+    public class NorthwindContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
 
     }
 }
