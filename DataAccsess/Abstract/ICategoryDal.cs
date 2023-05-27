7ab1ea4361221a055ab1b2a3be072dd3d5d9c059
ibrahim <ibrahimulusal185@gmail.com>
2023-05-27 19:01:29 +0300

    Data Accses Layer Update and IEntityRepository Added

--- DataAccsess/Abstract/ICategoryDal.cs
+++ DataAccsess/Abstract/ICategoryDal.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DataAccsess.Abstract
 {
-    public interface IProductDal:IEntityRepository<Product>
+    public interface ICategoryDal:IEntityRepository<Category>
     {
        
     }
