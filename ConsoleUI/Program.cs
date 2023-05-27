f6d797cbb5d8c55506c9df5d68c8f4606907fa02
ibrahim <ibrahimulusal185@gmail.com>
2023-05-27 18:09:18 +0300

    Project Update

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using Business.Concrete;
+using DataAccsess.Concrete.InMemory;
+using Entities.Concrete;
+ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+
+foreach(Product product in productManager.GetAll())
+{
+    Console.WriteLine(product.ProductName);
+}
+//Console.WriteLine(String.Format("{0:3}|{1:3}|{2:20}|{3:000000000}|{4:00000}", product.ProductId, product.CategoryId, product.ProductName, product.UnitPrice, product.UnitInStock));
