c069967346d5c2e352008cabdea8a292efe6ad18
ibrahim <ibrahimulusal185@gmail.com>
2023-05-27 19:19:11 +0300

    EfProductDal and EfCategoryDal Class Added

---
+++ DataAccsess/Concrete/Entity Framework/EfProductDal.cs
@@ -0,0 +1,39 @@
+﻿using DataAccsess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccsess.Concrete.Entity_Framework
+{
+    public class EfProductDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+
+        }
+
+        public void Delete(Product entity)
+        {
+
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+
+        }
+    }
+}
