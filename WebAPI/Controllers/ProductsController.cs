60cf0ec28f823f3a8037ff86cda97c8c28f5de87
ibrahim <ibrahimulusal185@gmail.com>
2023-06-24 21:43:51 +0300

    WebAPI projeye dahil edildi.

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductsController : ControllerBase
+	{
+		[HttpGet]
+		public List<Product> GetProducts()
+		{
+			return new List<Product>
+			{
+				new Product{ ProductId = 1, ProductName = "Apple"},
+				new Product {ProductId = 2, ProductName = "Telephone"}
+			};
+		}
+	}
+}
