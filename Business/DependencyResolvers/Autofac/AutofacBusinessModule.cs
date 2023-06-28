367ed5a8edaa5a9e84f9873cfa3f107aac8423c8
ibrahim <ibrahimulusal185@gmail.com>
2023-06-28 23:54:01 +0300

    IOC ile güncellendi!

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+namespace Business.DependencyResolvers.Autofac
+{
+	public class AutofacBusinessModule : Module
+	{
+	}
+}
