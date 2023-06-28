aa97982248f2996bc760ffe7e2574c2011f50520
ibrahim <ibrahimulusal185@gmail.com>
2023-06-29 00:12:01 +0300

    api configrasyon yapıldı

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,4 +1,6 @@
+using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using Business.DependencyResolvers.Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,6 +17,10 @@ public class Program
 	public static IHostBuilder CreateHostBuilder(string[] args) =>
 		Host.CreateDefaultBuilder(args)
 			.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+			.ConfigureContainer<ContainerBuilder>(builder =>
+			{
+				builder.RegisterModule(new AutofacBusinessModule());
+			})
 			.ConfigureWebHostDefaults(webBuilder =>
 			{
 				webBuilder.ConfigureServices(services =>
