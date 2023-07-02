5389c70f5e611c7e123adf78fe5bc8ed58153463
ibrahim <ibrahimulusal185@gmail.com>
2023-07-02 22:57:31 +0300

    Build Edildi!

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -8,10 +8,10 @@ ProductTest();
 //CategoryTest();
 static void ProductTest()
 {
-	ProductManager productManager = new ProductManager(new EfProductDal());
+	ProductManager productManager = new ProductManager(new EfProductDal(),new CategoryManager(new EfCategoryDal()));
 
 
-	foreach (var product in productManager.GetProductDetails())
+	foreach (var product in productManager.GetProductDetails().Data)
 	{
 		Console.WriteLine(String.Format("{0:0000}  {1:30}  {2:30}  {3:0000000000}", product.ProductId, product.CategoryName, product.ProductName, product.UnitsInStock));
 	}
@@ -20,11 +20,11 @@ static void ProductTest()
 static void CategoryTest()
 {
 	CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-	foreach (var category in categoryManager.GetAll())
+	foreach (var category in categoryManager.GetAll().Data)
 	{
 		Console.WriteLine(category.CategoryName);
 	}
 }
 CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
 
-Console.WriteLine(categoryManager.GetById(1).CategoryName);
+Console.WriteLine(categoryManager.GetById(1).Data.CategoryName);
