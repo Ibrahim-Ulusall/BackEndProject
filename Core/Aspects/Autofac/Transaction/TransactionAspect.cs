79d19831467b09450903d794df103917fa8a3ed0
ibrahim <ibrahimulusal185@gmail.com>
2023-08-28 21:13:40 +0300

    Transaction Eklendi

---
+++ Core/Aspects/Autofac/Transaction/TransactionAspect.cs
@@ -0,0 +1,26 @@
+﻿using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using System.Transactions;
+
+namespace Core.Aspects.Autofac.Transaction
+{
+	public class TransactionAspect:MethodInterception
+	{
+		public override void Intercept(IInvocation invocation)
+		{
+			using (TransactionScope scope = new TransactionScope())
+			{
+				try
+				{
+					invocation.Proceed();
+					scope.Complete();
+				}
+				catch (Exception e)
+				{
+					scope.Dispose();
+					throw;
+				}
+			}
+		}
+	}
+}
