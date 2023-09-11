d084e27f0a6b4797dd6a97e43c1e981fa419ca85
ibrahim <ibrahim@gmail.com>
2023-09-11 14:58:48 +0300

    Log4Net Eklendi

---
+++ Core/CrossCuttingConcerns/Logging/Log4Net/LogService.cs
@@ -0,0 +1,45 @@
+﻿using log4net;
+
+namespace Core.CrossCuttingConcerns.Logging.Log4Net
+{
+    public class LogService
+    {
+        private ILog _log;
+
+        public LogService(ILog log)
+        {
+            _log = log;
+        }
+        public bool IsInfoEnabled => _log.IsInfoEnabled;
+        public bool IsDebugEnabled => _log.IsDebugEnabled;
+        public bool IsFatalEnabled => _log.IsFatalEnabled;
+        public bool IsErrorEnabled => _log.IsErrorEnabled;
+        public bool IsWarnEnabled => _log.IsWarnEnabled;
+
+        public void Info(object logMessage)
+        {
+            if (IsInfoEnabled)
+                _log.Info(logMessage);
+        }
+        public void Warn(object logMessage)
+        {
+            if (IsWarnEnabled)
+                _log.Warn(logMessage);
+        }
+        public void Error(object logMessage)
+        {
+            if (IsErrorEnabled)
+                _log.Error(logMessage);
+        }
+        public void Fatal(object logMessage)
+        {
+            if (IsFatalEnabled)
+                _log.Fatal(logMessage);
+        }
+        public void Debug(object logMessage)
+        {
+            if (IsFatalEnabled)
+                _log.Debug(logMessage);
+        }
+    }
+}
